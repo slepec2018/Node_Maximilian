5715864d17f5f99d9a22553f5be5d8185490de91
slepec <slepec2018@gmail.com>
2023-06-03 19:40:52 +0100

    13_Mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl, null, req.user._id);
+  const product = new Product({title: title, price: price, description: description, imageUrl: imageUrl, userId: req.user});
   product.save()
     .then(result => {
       // console.log(result);
@@ -54,15 +54,13 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc,
-    updatedImageUrl,
-    prodId
-  );
-  product
-  .save()
+  Product.findById(prodId).then(product => { 
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.imageUrl = updatedImageUrl;
+    product.description = updatedDesc;
+    return product.save();
+  })
   .then(result => {
     console.log('UPDATED PRODUCT!');
     res.redirect('/admin/products');
@@ -71,7 +69,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
+    .populate('userId')
     .then(products => {
       res.render('admin/products', {
         prods: products,
@@ -84,7 +83,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
